6bf629b9fda0919c874a0c63d79c238cc23a0b25
Baptiste <baptiste.salvio@gmail.com>
2026-02-10 16:12:03 +0100

    transfert du contenu du fichier local vers le github

--- app/src/main/java/com/robin/polyhome/MainActivity.kt
+++ app/src/main/java/com/robin/polyhome/MainActivity.kt
@@ -1,20 +1,40 @@
-package com.robin.polyhome
+package com.salvio.polyhome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val buttonNewUser = findViewById<Button>(R.id.bttnNwUser)
+
+        val buttonConnexion = findViewById<Button>(R.id.buttonConnexion)
+
+        buttonConnexion.setOnClickListener {
+            val intentLogin = Intent(this, LoginActivity::class.java)
+            startActivity(intentLogin)
+        }
+
+        buttonNewUser.setOnClickListener {
+            val intentRegister = Intent(this, RegisterActivity::class.java)
+            startActivity(intentRegister)
+        }
+
+
     }
 }
